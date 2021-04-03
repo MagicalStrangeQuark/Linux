2fb09e0a2d8ad127055fc42bea5b6bcb54b5e5ec
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-04-02 22:22:34 -0300

    Update PackagesManjaro.sh

--- LinuxPackages/PackagesManjaro.sh
+++ LinuxPackages/PackagesManjaro.sh
@@ -4,6 +4,14 @@ sudo snap install circleci
 ## COMPOSER
 sudo pacman -Sy composer
 
+## KUBECTL
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(<kubectl.sha256) kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
+rm kubectl.sha256
+
 ## DIA
 sudo pacman -Sy dia
 
